24c68c730d8c34296fb041a8104932938567a9d2
POPPER <77334085+FYI-PSA@users.noreply.github.com>
2025-05-23 14:50:40 +0330

    Update main.py

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ def main(token, adminid):
 
     application.add_error_handler(error_handler)
 
-     application.bot.send_message(chat_id=update.effective_chat.id, text)
+     application.bot.send_message(chat_id=adminid, text="The bot has started!")
     while True:
         application.run_polling()
         print("Mainloooop...")
