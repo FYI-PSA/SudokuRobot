b073ed2a3604f56975cb91a1f36c90ece97285f8
POPPER <77334085+FYI-PSA@users.noreply.github.com>
2025-05-24 15:42:43 +0330

    less obvious stuff

--- sudokuimagetool.py
+++ sudokuimagetool.py
@@ -266,7 +266,7 @@ def rectangles_to_square_image(rectangle_boxes: list, rgb_image: np.ndarray, deb
     boundingbox_square = largest_square_bounding_from_list_of_rectangles(rectangle_boxes, debug=debug)
     corrected_boundary = ensure_square_boundary(boundingbox_square)  # ensures that width and height are the exact same number, takes an average weighted more towards the bigger guy
     square_image = extract_square_boundary_to_image(corrected_boundary, rgb_image, debug=debug)
-    print(f'bb: {boundingbox_square}\ncorrected: {corrected_boundary}\nimgshape: {np.shape(square_image)}\n\n')
+    # print(f'bb: {boundingbox_square}\ncorrected: {corrected_boundary}\nimgshape: {np.shape(square_image)}\n\n')
     return (square_image, corrected_boundary)
 
 
