599692591f24595c3e5d8eb40818bb30f94094db
POPPER <77334085+FYI-PSA@users.noreply.github.com>
2025-05-25 12:23:57 +0330

    Update sudokuimagetool.py

--- sudokuimagetool.py
+++ sudokuimagetool.py
@@ -332,13 +332,13 @@ def generate_grid(tiles: list, size: tuple, mostly_black: bool = False) -> Image
     picdict = {}
     DIRECTORY = 'numbers/'
     side = 0
-    border_thick = 15
+    border_thick = 13
 
     checked_side = False
     for n in range(0, 10):
         key = f'{n}.png'
         image = Image.open(DIRECTORY+key)
-        image = ImageOps.expand(image, border=border_thick*4, fill='white')
+        image = ImageOps.expand(image, border=border_thick*3, fill='white')
         image = ImageOps.expand(image, border=border_thick, fill='black')
         if not checked_side:
             side = image.width
@@ -358,11 +358,17 @@ def generate_grid(tiles: list, size: tuple, mostly_black: bool = False) -> Image
         
         new_tile = np.asarray(c_tile, dtype=np.uint8)[:, :, [2, 1, 0]]  # BGR mode for CV2
         
-        if (col % 3 == 2) and (col != 8):
+	if (col % 3 == 2) and (col != 8) and (row % 3 == 2) and (row != 8):
+            new_tile = cv2.copyMakeBorder(new_tile, 0, border_thick*4, 0, border_thick*4, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+	    new_tile = cv2.copyMakeBorder(new_tile, border_tick*4, 0, border_thick*4, 0, cv2.BORDER_CONSTANT, value=(255, 255, 255))
+
+        elif (col % 3 == 2) and (col != 8):
             new_tile = cv2.copyMakeBorder(new_tile, 0, 0, 0, border_thick*4, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+	    new_tile = cv2.copyMakeBorder(new_tile, border_tick*4, border_thick*4, border_thick*4, 0, cv2.BORDER_CONSTANT, value=(255, 255, 255))
         
-        if (row % 3 == 2) and (row != 8):
+        elif (row % 3 == 2) and (row != 8):
             new_tile = cv2.copyMakeBorder(new_tile, 0, border_thick*4, 0, 0, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+	    new_tile = cv2.copyMakeBorder(new_tile, border_tick*4, 0, border_thick*4, border_thick*4, cv2.BORDER_CONSTANT, value=(255, 255, 255))
         
         new_tile = Image.fromarray(new_tile[:, :, [2, 1, 0]])  # Back to RGB mode for Pillow
         new_tile = new_tile.resize((side, side), Image.LANCZOS)
@@ -370,12 +376,13 @@ def generate_grid(tiles: list, size: tuple, mostly_black: bool = False) -> Image
         image.paste(new_tile, (col*side, row*side))
         del new_tile
         
-        gc.collect()
+    gc.collect()
     image = ImageOps.expand(image, border=border_thick*2, fill='black')
     image = image.resize(size, Image.LANCZOS)
     if mostly_black:
         image = ImageOps.invert(image)
     return image
+
 
 
 def write_solved_grid_to_image(newfilename: str, filename: str, tile_list: list) -> Tuple[Tuple[int, int, int, int], Image.Image, Image.Image, bool]: 
